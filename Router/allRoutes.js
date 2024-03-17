e6d76dd3239f71a0569143610efa9f4b218b1de3
nancyh13500 <nancy.hanoyan@free.fr>
2024-03-17 14:29:29 +0100

    mise a jour allRoutes

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,7 +7,7 @@ export const allRoutes = [
     new Route("/actualites", "Actualités", "/pages/actualites.html"),
     new Route("/prochainement", "Prochain atelier", "/pages/prochainement.html"),
     new Route("/apropos", "A propos", "/pages/apropos.html"),
-    new Route("/contact", "Nous contacter", "/pages/contact.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
